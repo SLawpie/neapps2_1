1891f17625158e676f84a9712eccd894f72b426f
SLawpie <slawpie@hotmail.com>
2023-12-23 11:30:17 +0100

    spatie-permissions: installed

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRole;
+    use HasRole, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
